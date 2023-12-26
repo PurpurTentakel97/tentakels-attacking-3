9176dc0912e5ceceeda1db83a3a19711c7b13e3a
Martin <martin.schreurs@netcologne.de>
2023-12-26 22:51:10 +0100

    ide things and comments in appcontext

--- src/app_context/AppContext.cpp
+++ src/app_context/AppContext.cpp
@@ -11,100 +11,141 @@
 
 
 AppContext_ty AppContext::GetInstance() {
-	static AppContext appContext;
-	return appContext;
+    static AppContext appContext;
+    return appContext;
 }
 
-void AppContext::LoadConfig() {
-	::LoadConfig();
-	if (constants.sound.muteVolume) {
-		SetMasterVolume(0.0f);
-	}
-	else {
-		SetMasterVolume(constants.sound.masterVolume / 100);
-	}
-
-	SetTargetFPSEvent const event{ constants.window.FPS };
-	eventManager.InvokeEvent(event);
-
-}
 void AppContext::LoadLanguages() {
-	languageManager.Initialize();
+    languageManager.Initialize();
 }
+
+void AppContext::LoadConfig() const {
+    ::LoadConfig();
+    if (constants.sound.muteVolume) {
+        SetMasterVolume(0.0f);
+    } else {
+        SetMasterVolume(constants.sound.masterVolume / 100);
+    }
+
+    SetTargetFPSEvent const event{ constants.window.FPS };
+    eventManager.InvokeEvent(event);
+}
+
 void AppContext::SaveConfig() {
-	::SaveConfig();
+    ::SaveConfig();
 }
 
 void AppContext::ValidateConfig() {
-	// Global
-	ValidateMinMax<size_t>(constants.global.minRounds, constants.global.maxRounds,
-		"Min Game Rounds", "Max Game Rounds");
-	ValidateMinCurrentMax<size_t>(constants.global.minRounds, constants.global.currentTargetRound,
-		constants.global.maxRounds);
+    // Global
+    ValidateMinMax<size_t>(
+            constants.global.minRounds,
+            constants.global.maxRounds,
+            "Min Game Rounds",
+            "Max Game Rounds"
+    );
+    ValidateMinCurrentMax<size_t>(
+            constants.global.minRounds,
+            constants.global.currentTargetRound,
+            constants.global.maxRounds
+    );
 
-	// Player
-	ValidateMinMax<size_t>(constants.player.minPlayerCount, constants.player.maxPlayerCount,
-		"Min Player Count", "Max Player Count");
+    // Player
+    ValidateMinMax<size_t>(
+            constants.player.minPlayerCount,
+            constants.player.maxPlayerCount,
+            "Min Player Count",
+            "Max Player Count"
+    );
 
-	// World
-	ValidateMinMax<size_t>(constants.world.minPlanetCount, constants.world.maxPlanetCount,
-		"Min Planet Count", "Max Planet Count");
-	ValidateMinCurrentMax<size_t>(constants.world.minPlanetCount, constants.world.currentPlanetCount,
-		constants.world.maxPlanetCount);
+    // World
+    ValidateMinMax<size_t>(
+            constants.world.minPlanetCount,
+            constants.world.maxPlanetCount,
+            "Min Planet Count",
+            "Max Planet Count"
+    );
+    ValidateMinCurrentMax<size_t>(
+            constants.world.minPlanetCount,
+            constants.world.currentPlanetCount,
+            constants.world.maxPlanetCount
+    );
 
-	ValidateMinMax<int>(constants.world.minDimensionX, constants.world.maxDimensionX,
-		"Min World Width", "Max World Width");
-	ValidateMinCurrentMax<int>(constants.world.minDimensionX, constants.world.currentDimensionX,
-		constants.world.maxDimensionX);
+    ValidateMinMax<int>(
+            constants.world.minDimensionX,
+            constants.world.maxDimensionX,
+            "Min World Width",
+            "Max World Width"
+    );
+    ValidateMinCurrentMax<int>(
+            constants.world.minDimensionX,
+            constants.world.currentDimensionX,
+            constants.world.maxDimensionX
+    );
 
-	ValidateMinMax<int>(constants.world.minDimensionY, constants.world.maxDimensionY,
-		"Min World Height", "Max World Height");
-	ValidateMinCurrentMax<int>(constants.world.minDimensionY, constants.world.currentDimensionY,
-		constants.world.maxDimensionY);
+    ValidateMinMax<int>(
+            constants.world.minDimensionY,
+            constants.world.maxDimensionY,
+            "Min World Height",
+            "Max World Height"
+    );
+    ValidateMinCurrentMax<int>(
+            constants.world.minDimensionY,
+            constants.world.currentDimensionY,
+            constants.world.maxDimensionY
+    );
 
-	// Sound
-	ValidateLowerEqual<float>(constants.sound.masterVolume, 100.0f, "Master Volume");
-	ValidateGreaterEqual<float>(constants.sound.masterVolume, 0.0f, "Master Volume");
+    // Sound
+    ValidateLowerEqual<float>(constants.sound.masterVolume, 100.0f, "Master Volume");
+    ValidateGreaterEqual<float>(constants.sound.masterVolume, 0.0f, "Master Volume");
 
-	// Planet
-	ValidateLowerEqual<float>(constants.planet.homeworldSpacing, 1.0f, "Homeworld Spacing");
-	ValidateGreaterEqual<float>(constants.planet.homeworldSpacing, 0.0f, "Homeworld Spacing");
+    // Planet
+    ValidateLowerEqual<float>(constants.planet.homeworldSpacing, 1.0f, "Homeworld Spacing");
+    ValidateGreaterEqual<float>(constants.planet.homeworldSpacing, 0.0f, "Homeworld Spacing");
 
-	ValidateLowerEqual<float>(constants.planet.globalSpacing, 1.0f, "Global Spacing");
-	ValidateGreaterEqual<float>(constants.planet.globalSpacing, 0.0f, "Global Spacing");
+    ValidateLowerEqual<float>(constants.planet.globalSpacing, 1.0f, "Global Spacing");
+    ValidateGreaterEqual<float>(constants.planet.globalSpacing, 0.0f, "Global Spacing");
 
-	// Fleet
-	ValidateMinMax<int>(constants.fleet.minFleetSpeed, constants.fleet.maxFleetSpeed, "Min Fleet Movement", "Max Fleet Movement");
-	ValidateMinCurrentMax<int>(constants.fleet.minFleetSpeed, constants.fleet.currentFleetSpeed, constants.fleet.maxFleetSpeed);
+    // Fleet
+    ValidateMinMax<int>(
+            constants.fleet.minFleetSpeed,
+            constants.fleet.maxFleetSpeed,
+            "Min Fleet Movement",
+            "Max Fleet Movement"
+    );
+    ValidateMinCurrentMax<int>(
+            constants.fleet.minFleetSpeed,
+            constants.fleet.currentFleetSpeed,
+            constants.fleet.maxFleetSpeed
+    );
 
-	Print(PrintType::INFO, "Config validated");
+    Print(PrintType::INFO, "Config validated");
 }
 
 void AppContext::OnEvent(Event const& event) {
 
-	if (auto const LastRoundEvent = dynamic_cast<SetCurrentLastRoundEvent const*>(&event)) {
-		constants.global.currentTargetRound = LastRoundEvent->GetLastRound();
-		return;
-	}
+    if (auto const LastRoundEvent = dynamic_cast<SetCurrentLastRoundEvent const*>(&event)) {
+        constants.global.currentTargetRound = LastRoundEvent->GetLastRound();
+        return;
+    }
 }
 
 Vector2 AppContext::GetResolution() const {
-	return constants.window.currentResolutionVec;
+    return constants.window.currentResolutionVec;
 }
 
 AppContext::AppContext() {
-	eventManager.AddListener(&soundManager);
-	eventManager.AddListener(&playerCollection);
-	eventManager.AddListener(&languageManager);
-	eventManager.AddListener(this);
+    eventManager.AddListener(&soundManager);
+    eventManager.AddListener(&playerCollection);
+    eventManager.AddListener(&languageManager);
+    eventManager.AddListener(this);
 
-	Print(PrintType::INITIALIZE, "AppContext");
+    Print(PrintType::INITIALIZE, "AppContext");
 }
 
 AppContext::~AppContext() {
-	eventManager.RemoveListener(&soundManager);
-	eventManager.RemoveListener(&playerCollection);
-	eventManager.RemoveListener(&languageManager);
-	eventManager.RemoveListener(this);
-	Print(PrintType::INFO, "AppContext deleted");
+    eventManager.RemoveListener(&soundManager);
+    eventManager.RemoveListener(&playerCollection);
+    eventManager.RemoveListener(&languageManager);
+    eventManager.RemoveListener(this);
+    Print(PrintType::INFO, "AppContext deleted");
 }
--- src/app_context/include/AppContext.hpp
+++ src/app_context/include/AppContext.hpp
@@ -17,67 +17,33 @@
 #include <helper/HSoundManager.hpp>
 #include <string>
 
-/**
- * contains all global variables of the game.
- * contains all assets and colors.
- * contains the event management.
- * is a singleton.
- */
 struct AppContext final : public EventListener {
 public:
-    SoundManager soundManager;         ///< loads and manage all sounds
-    AssetManager assetManager;         ///< loads and manage all assets
-    HLanguageManager languageManager;  /// loads and manages all language things
-    EventManager eventManager;         ///< manage the EventListener and invokes events
-    PlayerCollection playerCollection; ///< contains non logic info's about player
-    Colors colors;                     ///< contains all colors and check valid color
-    Constants constants;               ///< contains all constants of the game
+    SoundManager soundManager;
+    AssetManager assetManager;
+    HLanguageManager languageManager;
+    EventManager eventManager;
+    PlayerCollection playerCollection;
+    Colors colors;
+    Constants constants;
 
-
-    /**
-	 * creates a Singleton.
-	 * returns an instance.
-	 */
     [[nodiscard]] static AppContext_ty GetInstance();
 
-    /**
-	 * loads the languages.
-	 */
     void LoadLanguages();
 
-    /**
-	 * loads config.
-	 * if no config exists a config is generated.
-	 * validate loaded data.
-	 */
-    void LoadConfig();
-    /**
-	 * saves the config.
-	 * is also used to generate a config if no config exists.
-	 */
-    void SaveConfig();
+    void LoadConfig() const;
 
-    /**
-	 * validate all constants in constants that can be loaded by the config.
-	 */
+    static void SaveConfig();
+
     void ValidateConfig();
-    /**
-	 * allows only arithmetic types.
-	 * validate if a current value is between min and max.
-	 * if not: the current value is set to the edge case.
-	 */
+
     template<arithmetic D>
     inline void ValidateMinCurrentMax(D min, D& current, D max) const {
 
         current = min <= current ? current : min;
         current = max >= current ? current : max;
     }
-    /**
-	 * allows only arithmetic types.
-	 * validate if lhs is smaller than rhs.
-	 * if not: rhs is set to lhs +1.
-	 * generates a popup if rhs gets set.
-	 */
+
     template<arithmetic D>
     inline void ValidateMinMax(D& lhs, D& rhs, std::string const& lhsMessage, std::string const& rhsMessage) const {
 
@@ -92,12 +58,7 @@ public:
                                            []() {} };
         eventManager.InvokeEvent(event);
     }
-    /**
-	 * allows only arithmetic types.
-	 * validate if a value is smaller than or even to max.
-	 * if not: value is set to max.
-	 * generates a popup if value gets set.
-	 */
+
     template<arithmetic D>
     inline void ValidateLowerEqual(D& value, D max, std::string const& valueMessage) const {
 
@@ -112,12 +73,7 @@ public:
                                            []() {} };
         eventManager.InvokeEvent(event);
     }
-    /**
-	 * allows only arithmetic types.
-	 * validate if a value is greater than or even to min.
-	 * if not: value is set to min.
-	 * generates a popup if value gets set.
-	 */
+
     template<arithmetic D>
     inline void ValidateGreaterEqual(D& value, D min, std::string const& valueMessage) const {
 
@@ -133,15 +89,8 @@ public:
         eventManager.InvokeEvent(event);
     }
 
-    /**
-	 * receives the events vom the manager.
-	 * checks if the expected event is deployed if so it calls a member function.
-	 */
     void OnEvent(Event const& event) override;
 
-    /**
-	 * returns the current resolution.
-	 */
     [[nodiscard]] Vector2 GetResolution() const;
 
     AppContext(AppContext const&) = delete;
@@ -150,14 +99,7 @@ public:
     AppContext& operator=(AppContext&&) = delete;
 
 private:
-    /**
-	 * private Constructor so that the member function GetInstance()
-	 * is the only place were an instance can be created.
-	 */
     AppContext();
 
-    /**
-	 * just vor debugging
-	 */
     ~AppContext() override;
 };
