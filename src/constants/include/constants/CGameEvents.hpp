b8e4ab788baacb775d15ba9b064af7db5adf23e8
Martin <martin.schreurs@netcologne.de>
2023-12-26 21:18:28 +0100

    include brackets constants

--- src/constants/include/constants/CGameEvents.hpp
+++ src/constants/include/constants/CGameEvents.hpp
@@ -4,9 +4,9 @@
 //
 
 #pragma once
-#include "helper/HGameEventTypes.hpp"
 #include <array>
 #include <cstddef>
+#include <helper/HGameEventTypes.hpp>
 
 /**
  * contains the constants for the game events.
@@ -17,14 +17,14 @@ struct CGameEvents {
     }; ///< config load checks if the count is idetical to the loaded entry count
 
     HGameEventType events{ 0b11111111 }; ///< contains the bismask for the events --> get overwritten by config
-	float globalEventChance{ 25.0f }; ///< contains the chance a event is happening --> get overwritten by config
+    float globalEventChance{ 25.0f };    ///< contains the chance a event is happening --> get overwritten by config
 
-	/**
+    /**
 	 * sets the bitflag for a specific game event type.
 	 */
-	void SetFlag(HGameEventType const type, bool const active);
-	/**
+    void SetFlag(HGameEventType const type, bool const active);
+    /**
 	 * return if the flag for a specific game event type is set.
 	 */
-	[[nodiscard]] bool IsFlag(HGameEventType const type) const;
+    [[nodiscard]] bool IsFlag(HGameEventType const type) const;
 };
