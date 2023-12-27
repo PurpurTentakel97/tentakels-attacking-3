ce7e5dd4d3560024082d14f7b766f05819616602
Martin <martin.schreurs@netcologne.de>
2023-12-27 01:21:31 +0100

    comments and ide things in events

--- src/event/include/event/EventManager.hpp
+++ src/event/include/event/EventManager.hpp
@@ -8,38 +8,22 @@
 #include <algorithm>
 #include <vector>
 
-
-/**
- * manages the listener.
- * provides all events to the listeners.
- */
- class  EventManager final {
+class EventManager final {
 public:
-	/**
-	 * adds a listener so it can receive all events.
-	 */
-	void AddListener(EventListener* eventListener) {
-		m_listeners.push_back(eventListener);
-	}
+    void AddListener(EventListener* eventListener) {
+        m_listeners.push_back(eventListener);
+    }
 
-	/**
-	 * remove's a listener so events gets no longer provided to this listener.
-	 */
-	void RemoveListener(EventListener* eventListener) {
-		m_listeners.erase(std::remove(
-			m_listeners.begin(), m_listeners.end(), eventListener),
-			m_listeners.end());
-	}
+    void RemoveListener(EventListener* eventListener) {
+        m_listeners.erase(std::remove(m_listeners.begin(), m_listeners.end(), eventListener), m_listeners.end());
+    }
 
-	/**
-	 * provides the current event to all listeners. 
-	 */
-	void InvokeEvent(Event const& event) const {
-		for (auto const listener : m_listeners) {
-			listener->OnEvent(event);
-		}
-	}
+    void InvokeEvent(Event const& event) const {
+        for (auto const listener : m_listeners) {
+            listener->OnEvent(event);
+        }
+    }
 
 private:
-	std::vector<EventListener*> m_listeners{ }; ///< contains all active listeners
+    std::vector<EventListener*> m_listeners{};
 };
