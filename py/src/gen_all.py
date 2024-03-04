720690cbe61a3a56c8c7208836c37bd06cdfdaa4
Martin <martin.schreurs@netcologne.de>
2024-03-04 22:38:33 +0100

    gen all

---
+++ py/src/gen_all.py
@@ -0,0 +1,19 @@
+#
+# Purpur Tentakel
+# 02.03.2024
+#
+
+import file
+import raw_field
+import raw_config_file
+import gen_config_classes
+import gen_config_enum
+import gen_config_io
+
+
+def gen(fields: tuple[raw_field.RawField], config: tuple[raw_config_file.RawConfigFile]) -> tuple[file.File]:
+    generated: list = list()
+    generated.extend(gen_config_classes.gen(fields, config))
+    generated.extend(gen_config_enum.gen(fields, config))
+    generated.extend(gen_config_io.gen(fields, config))
+    return tuple(generated)
