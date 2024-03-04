540892fdad74371db74b95e52a73ea53cfd4098c
Martin <martin.schreurs@netcologne.de>
2024-03-04 23:14:56 +0100

    gen config io header

--- py/src/gen_config_io.py
+++ py/src/gen_config_io.py
@@ -13,7 +13,14 @@ import include
 
 def gen_header() -> file.File:
     indent: int = 1
-    text: str = ""
+    text: str = f"{helper.indent(indent)}struct {helper.config_io_name} final {helper.left_bracket}\n"
+
+    indent += 1
+    text += f"{helper.indent(indent)}static void LoadConfig();\n\n"
+    text += f"{helper.indent(indent)}static void SaveConfig();\n"
+
+    indent -= 1
+    text += f"{helper.indent(indent)}{helper.right_bracket};\n"
 
     return file.File(helper.config_io_name, enums.FileType.HEADER, [], "cst", text)
 
