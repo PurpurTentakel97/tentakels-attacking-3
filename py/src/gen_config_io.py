63a1bd445ace3070e7673e7cbf9bc17e952fedf0
Martin <martin.schreurs@netcologne.de>
2024-03-05 21:30:19 +0100

    first gen of config io

--- py/src/gen_config_io.py
+++ py/src/gen_config_io.py
@@ -3,12 +3,12 @@
 # 02.03.2024
 #
 
-import raw_field
-import raw_config_file
+import enums
 import file
 import helper
-import enums
 import include
+import raw_config_file
+import raw_field
 
 
 def gen_header() -> file.File:
@@ -26,11 +26,39 @@ def gen_header() -> file.File:
 
 
 def gen_source(fields: tuple[raw_field.RawField], config_files: tuple[raw_config_file.RawConfigFile]) -> file.File:
+    constants: str = "constants"
     indent: int = 1
-    text: str = ""
+    load_text: str = f"{helper.indent(indent)}void {helper.config_io_name}::LoadConfig() {helper.left_bracket}\n"
+    save_text: str = f"{helper.indent(indent)}void {helper.config_io_name}::SaveConfig() {helper.left_bracket}\n"
+
+    indent += 1
+
+    # setup
+    load_text += f"{helper.indent(indent)}loadEntryCount = 0;\n{helper.indent(indent)}nlohmann::json load;\n" \
+                 f"{helper.indent(indent)}auto& {constants} = app::AppContext::GetInstance().{constants};\n" \
+                 f"{helper.indent(indent)}if (not LoadAndValidateConfigJson(load)) {helper.left_bracket}\n"
+    indent += 1
+    load_text += f"{helper.indent(indent)}return;\n"
+    indent -= 1
+    load_text += f"{helper.indent(indent)}{helper.right_bracket}\n\n"
+    save_text += f"{helper.indent(indent)}auto const& {constants} = app::AppContext::GetInstance().{constants};\n" \
+                 f"{helper.indent(indent)}nlohmann::json save;\n\n"
+
+    # gen
+
+    # end
+    load_text += f"{helper.indent(indent)}CheckLoadEntryCount();\n" \
+                 f"{helper.indent(indent)}hlp::Print(hlp::PrintType::INFO, \"config loaded\");\n"
+    save_text += f"{helper.indent(indent)}hlp::SaveFile(Files::s_savesDir, Files::s_configFile, save.dump(4));\n" \
+                 f"{helper.indent(indent)}hlp::Print(hlp::PrintType::INFO, \"config saved\");\n"
+
+    indent -= 1
+    load_text += f"{helper.indent(indent)}{helper.right_bracket}\n"
+    save_text += f"{helper.indent(indent)}{helper.right_bracket}\n"
 
     return file.File(helper.config_io_name, enums.FileType.SOURCE,
-                     [include.Include(f"{helper.config_io_name}.hpp", False)], "cst", text)
+                     [include.Include(f"{helper.config_io_name}.hpp", False),
+                      include.Include("HelperConfigIO.hpp", False)], "cst", f"{load_text}\n{save_text}")
 
 
 def gen(fields: tuple[raw_field.RawField], config_files: tuple[raw_config_file.RawConfigFile]) -> tuple[
